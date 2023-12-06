dbfc62225db6277757c9bc31f09afedf2ab8e737
christoff-linde <christoffl.linde@icloud.com>
2023-12-06 07:59:56 +0200

    feat(day 01): solve day 01 part 02

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -22,8 +22,44 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(output)
 }
 
+fn process_line(line: &str) -> u32 {
+    let mut it = (0..line.len()).filter_map(|index| {
+        let reduced_line = &line[index..];
+        let result = if reduced_line.starts_with("one") {
+            Some(1)
+        } else if reduced_line.starts_with("two") {
+            Some(2)
+        } else if reduced_line.starts_with("three") {
+            Some(3)
+        } else if reduced_line.starts_with("four") {
+            Some(4)
+        } else if reduced_line.starts_with("five") {
+            Some(5)
+        } else if reduced_line.starts_with("six") {
+            Some(6)
+        } else if reduced_line.starts_with("seven") {
+            Some(7)
+        } else if reduced_line.starts_with("eight") {
+            Some(8)
+        } else if reduced_line.starts_with("nine") {
+            Some(9)
+        } else {
+            reduced_line.chars().next().unwrap().to_digit(10)
+        };
+
+        result
+    });
+    let first = it.next().expect("should be a number");
+
+    match it.last() {
+        Some(num) => first * 10 + num,
+        None => first * 10 + first,
+    }
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let result = input.lines().map(process_line).sum::<u32>();
+    Some(result)
 }
 
 #[cfg(test)]
