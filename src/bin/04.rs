0aa147c11f22aeabeaff8511ae8bfc488b1b1ccd
christoff-linde <christoffl.linde@icloud.com>
2023-12-30 16:18:20 +0200

    feat: day 04 pt 1 complete

---
+++ src/bin/04.rs
@@ -0,0 +1,84 @@
+advent_of_code::solution!(4);
+
+use std::collections::HashSet;
+
+use nom::{
+    bytes::complete::tag,
+    character::complete::{self, digit1, line_ending, space0, space1},
+    multi::{fold_many1, separated_list1},
+    sequence::{delimited, separated_pair, terminated, tuple},
+    IResult, Parser,
+};
+
+#[derive(Debug)]
+struct Card {
+    winning_numbers: HashSet<u32>,
+    my_numbers: HashSet<u32>,
+}
+
+impl Card {
+    fn score(&self) -> u32 {
+        let power = self.winning_numbers.intersection(&self.my_numbers).count() as u32;
+
+        match power.checked_sub(1) {
+            Some(num) => 2u32.pow(num),
+            None => 0,
+        }
+    }
+}
+
+fn set(input: &str) -> IResult<&str, HashSet<u32>> {
+    fold_many1(
+        terminated(complete::u32, space0),
+        HashSet::new,
+        |mut acc: HashSet<_>, item| {
+            acc.insert(item);
+            acc
+        },
+    )(input)
+}
+
+fn card(input: &str) -> IResult<&str, Card> {
+    let (input, _) = delimited(
+        tuple((tag("Card"), space1)),
+        digit1,
+        tuple((tag(":"), space1)),
+    )(input)?;
+    separated_pair(set, tuple((tag("|"), space1)), set)
+        .map(|(winning_numbers, my_numbers)| Card {
+            winning_numbers,
+            my_numbers,
+        })
+        .parse(input)
+}
+
+fn cards(input: &str) -> IResult<&str, Vec<Card>> {
+    separated_list1(line_ending, card)(input)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (_, card_data) = cards(input).expect("a valid parse");
+
+    Some(card_data.iter().map(|card| card.score()).sum::<u32>())
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(13));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
